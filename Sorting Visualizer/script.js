0756e675282ee58f3034e63a89a2e1ff15ba499e
Luca <lucaf204@gmail.com>
2024-09-12 13:37:55 +0200

    added insertionSort

--- Sorting Visualizer/script.js
+++ Sorting Visualizer/script.js
@@ -113,4 +113,37 @@ document.getElementById('bubbleSort').addEventListener('click',bubbleSort);
 
 async function insertionSort() {
     const bars = document.getElementsByClassName('array-bar');
+    for(let i=1; i< array.length;i++){
+        let insertNumber = array[i];
+        let j = i-1;
+
+        bars[i].style.backgroundColor = "red";
+
+        while(j>=0 && array[j] > insertNumber){
+            bars[j].style.backgroundColor= "yellow";
+
+            //shift upwards
+            array[j+1] = array[j];
+            bars[j+1].style.height = `${array[j]* 3}px`; //heihgt update
+
+            await delay(100 - speed);
+
+            bars[j].style.backgroundColor = "rgb(24, 190, 255)";
+
+            j = j - 1; // Move to the previous element
+        }
+
+        array[j+1] = insertNumber;
+        bars[j+1].style.height = `${insertNumber * 3}px`;
+
+        bars[j+1].style.backgroundColor= "lightgreen";
+
+        await delay(100 - speed);
+    }
+
+    for (let i = 0; i < array.length; i++) {
+        bars[i].style.backgroundColor = "lightgreen"; // Final sorted color
+    }
 }
+
+document.getElementById('insertionSort').addEventListener('click',insertionSort);
